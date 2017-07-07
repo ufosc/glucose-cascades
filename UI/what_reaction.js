e953c59383a7264d6043748d778c9b96b4fbb61d
Anthony Rossello <rossello.ac@gmail.com>
2017-07-07 17:55:30 -0400

    added load image and load title text function

--- UI/what_reaction.js
+++ UI/what_reaction.js
@@ -1,4 +1,18 @@
+// get question
+// get title text from question
+// get image from question
+// get choices from question
+// get correct choice from question
+
 var correctChoice = 1;
+
+function loadImage(image) {
+    document.getElementById("image").src = image;
+}
+
+function loadTitleText(text) {
+    document.getElementById("titleText").innerHTML = text;
+}
 
 function choiceIsSelected(choice) {
     if (choice == correctChoice) {
